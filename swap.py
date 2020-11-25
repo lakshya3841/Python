34bf0da4a6c0157b62dffd3a97baa725cd6627ac
Hamster <62415196+lakshya3841@users.noreply.github.com>
2020-11-25 18:45:53 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,12 @@
+print("Hello , World")
+a = 1 
+b = 9
+b = b-a
+a = a+b
+b = a-b
+print("a =",  a)
+print("b =", b)
+print(len("Hello"),type(a))
+x = [880,"hello","world"]
+x.append("lol")
+print(x)
