ada17ef32f51d4d3c0bb5df0107ff537cd25531f
Hamster <62415196+lakshya3841@users.noreply.github.com>
2020-12-02 15:28:54 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,4 @@
+i = 0 
+a = input("PLEASE ENTER THE RANGE YOU WANT EVEN NUMBERS ")
+for i in range(0 , a , 2):
+    print(i)
---
+++ odd.py
@@ -0,0 +1,4 @@
+i = 0
+a = input("PLEASE ENTER THE RANGE YOU WANT ME TO PRINT ODD NUMBERS ")
+for i in range(0 , a , 3):
+    print(i)
