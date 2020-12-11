54038fd7b738f25f28628c726728ef522d9e57e5
Hamster <62415196+lakshya3841@users.noreply.github.com>
2020-12-11 16:04:27 +0530

    Add files via upload

---
+++ list_fundamentals.py
@@ -0,0 +1,26 @@
+nums = [5,3,5,2,4]
+nums.append(5)#Appends the given data after the latest data
+print(nums[2:])
+nums.pop()#pops or removes the recent data item
+print(nums)
+print(nums[-1])#In opposite direction it works as negative too..NEGATIVE RANGE
+print(nums)
+#:)
+nums.pop()
+print(nums)
+print("new list starts")
+x = [21,30,53,1,7,8]
+print(min(x))
+print(max(x))
+x.sort()
+print(x)
+x.reverse()
+print(x)#The sorted sequence has been reversed
+x.sort()
+'''
+x.clear()
+print(x) THIS REMOVES ALL DATA FROM THE LIST
+'''
+x.append(21)
+print(x.count(21))
+print(sum(x))
